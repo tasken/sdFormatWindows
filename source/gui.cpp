cd7f551bb420426c6637394fcf273648240ed0b6
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2026-06-25 12:31:13 -0700

    gui: add button to refresh drive list

--- source/gui.cpp
+++ source/gui.cpp
@@ -56,6 +56,22 @@ static void CloseConsoleWindow(void)
     }
 }
 
+static void InitDriveListComboBox(HWND hwnd) {
+    // Initialize drive list
+    HWND comboBox = GetDlgItem(hwnd, IDC_DRIVELIST);
+    retrieveAllActiveDrives();
+    for(u32 i=0; i < allActiveDrives.size(); i++)
+    {
+        ComboBox_AddString(comboBox, (LONG_PTR)allActiveDrives.at(i).c_str());
+    }
+}
+
+static inline void ClearDriveListComboBox(HWND hwnd) {
+    HWND comboBox = GetDlgItem(hwnd, IDC_DRIVELIST);
+    ComboBox_ResetContent(comboBox);
+    allActiveDrives.clear();
+}
+
 static void OnFormatButtonClick(void) {
     switch(MessageBox(window, "This will ERASE ALL DATA on this drive! Press Yes to continue", "Warning", MB_YESNO | MB_ICONWARNING)) {
         case IDCANCEL:
@@ -90,13 +106,7 @@ static INT_PTR CALLBACK DialogProc(HWND hwnd, UINT message, WPARAM wParam, LPARA
 {
     switch(message) {
         case WM_INITDIALOG: {
-            // Initialize drive list
-            HWND comboBox = GetDlgItem(hwnd, IDC_DRIVELIST);
-            retrieveAllActiveDrives();
-            for(u32 i=0; i < allActiveDrives.size(); i++)
-            {
-                ComboBox_AddString(comboBox, (LONG_PTR)allActiveDrives.at(i).c_str());
-            }
+            InitDriveListComboBox(hwnd);
 
             // Initialize GUI icons
             SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON)));
@@ -105,6 +115,12 @@ static INT_PTR CALLBACK DialogProc(HWND hwnd, UINT message, WPARAM wParam, LPARA
             return TRUE;
         }
         case WM_COMMAND: {
+            if (HIWORD(wParam) == BN_CLICKED && LOWORD(wParam) == IDC_BUTTON_REFERSH_DRIVES) {
+                ClearDriveListComboBox(hwnd);
+                InitDriveListComboBox(hwnd);
+                break;
+            }
+
             if (HIWORD(wParam) == BN_CLICKED && LOWORD(wParam) == IDC_BUTTON_START) {
                 OnFormatButtonClick();
                 break;
